f7b51a604fb6fcccc6a24eb094772fd45300361e
Sidhanth <sidhanth.surana12@gmail.com>
2019-05-26 00:10:04 +0530

    night hackathon

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -17,11 +17,27 @@ export default function Home() {
 
                 <Col md='8'>
 
-                    <h1 className='font-effect-anaglyph'>Sidhanth Surana</h1>
-                    <p>A Full stack Javascript developer fueled purely by pizzas. A polyglot developer
+                    <h1 className='font-effect-anaglyph title'>Sidhanth Surana</h1>
+                    <p>A Full stack Javascript web and mobile developer fueled purely by pizzas. A polyglot developer
                          who loves tinkering and exploring new libraries , frameworks and languages. Core believer
                          of hardwork beating talent. Currently enjoying the MERN stack and GraphQL schematic for APIs.
                     </p>
+
+                    <div >
+                        <h1 className='title mt-4 mb-3'>Connect </h1>
+                        <div className='d-flex flex-row justify-content-between second-div'>
+                            <a href='https://github.com/Sidhanthsur'>
+                                <i class="fab fa-github fa-3x"></i>
+                            </a>
+
+                            <a href='https://www.linkedin.com/in/sidhanthsur/'>
+                                <i class="fab fa-linkedin fa-3x"></i>
+                            </a>
+                            <a href='https://medium.com/@sidhanth'>
+                                <i class="fab fa-medium fa-3x"></i>
+                            </a>
+                        </div>
+                    </div>
                 </Col>
 
             </Row>
